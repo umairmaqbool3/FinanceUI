05e849719ca19cd6c381d6ab30ab451b647a8250
umair <umair.maqbool55@gmail.com>
2025-12-28 22:50:36 +0500

    Change in data

--- constants/data.ts
+++ constants/data.ts
@@ -1,5 +1,5 @@
 export const data = [
-    { title: 'Salary', amount: '$2,000', duration: 'Monthly', },
-    { title: 'Groceries', amount: '$2,000', duration: 'Monthly', },
-    { title: 'Rent', amount: '$2,000', duration: 'Monthly', },
+    { title: 'Salary', amount: '$2,000', duration: 'Monthly', date: "18:27 - April 30" },
+    { title: 'Groceries', amount: '$2,000', duration: 'Pantry', date: "17:00 - April 24" },
+    { title: 'Rent', amount: '$2,000', duration: 'Rent', date: "08:30 - April 15" },
 ]

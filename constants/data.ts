998cad347b248b96e276b262ab2b5f91259a05de
umair <umair.maqbool55@gmail.com>
2026-01-03 22:20:41 +0500

    Layout

--- constants/data.ts
+++ constants/data.ts
@@ -5,5 +5,5 @@ import SalaryIcon from "../assets/svgs/SalaryIcon";
 export const data = [
     { title: 'Salary', amount: '$2,000', duration: 'Monthly', date: "18:27 - April 30", icon: SalaryIcon },
     { title: 'Groceries', amount: '$2,000', duration: 'Pantry', date: "17:00 - April 24", icon: GroceryIcon },
-    { title: 'Rent', amount: '-$100', duration: 'Rent', date: "08:30 - April 15", icon: RentIcon },
+    { title: 'Rent', amount: '-$100  ', duration: 'Rent', date: "08:30 - April 15", icon: RentIcon },
 ]

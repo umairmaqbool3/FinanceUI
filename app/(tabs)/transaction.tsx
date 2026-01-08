19f2df8ba6767e988350dbc741cac8444d72f6bb
umair <umair.maqbool55@gmail.com>
2026-01-08 22:04:53 +0500

    Worked on transaction screen

--- app/(tabs)/transaction.tsx
+++ app/(tabs)/transaction.tsx
@@ -1,15 +1,18 @@
 import Header from '@/components/Header';
 import Screen from '@/components/Screen';
 import { Colors } from '@/constants/theme';
+import { spacingX, spacingY } from '@/constants/theme1';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View, useWindowDimensions } from 'react-native';
 
 const TransactionScreen = () => {
     const theme = useColorScheme() ?? 'light';
     const router = useRouter();
+    const { width, height } = useWindowDimensions();
+
     return (
         <Screen style={{ backgroundColor: Colors[theme].primary, }}>
             <Header
@@ -21,10 +24,24 @@ const TransactionScreen = () => {
                 onRightPress={() => console.log('Notifications/Right pressed')}
             />
 
+            <View style={[styles.contentContainer, { height: height * (Platform.OS == 'ios' ? 0.55 : 0.55), backgroundColor: Colors[theme].secondary }]}>
+
+            </View>
+
         </Screen>
     );
 };
 
 export default TransactionScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    contentContainer: {
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        paddingHorizontal: spacingX._30,
+        paddingTop: spacingY._40,
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+    },
+});

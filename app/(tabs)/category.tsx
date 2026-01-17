fa1727fe15394df845a5d68efb4a7775e2362576
umair <umair.maqbool55@gmail.com>
2026-01-17 22:55:55 +0500

    Started working on categories screen

--- app/(tabs)/category.tsx
+++ app/(tabs)/category.tsx
@@ -1,18 +1,59 @@
+import BalanceComponent from '@/components/BalanceComponent';
+import Header from '@/components/Header';
 import Screen from '@/components/Screen';
 import { Colors } from '@/constants/theme';
+import { spacingX, spacingY } from '@/constants/theme1';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Platform, StyleSheet, useWindowDimensions, View } from 'react-native';
 
 const CategoryScreen = () => {
     const theme = useColorScheme() ?? 'light';
+    const { width, height } = useWindowDimensions();
+
     return (
-        <Screen style={{ backgroundColor: Colors[theme].primary, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ color: Colors[theme].text, fontSize: 20 }}>Category Screen</Text>
+        <Screen style={{ backgroundColor: Colors[theme].primary }}>
+            <Header
+                title="Categories"
+                theme={theme}
+                leftIcon={<Ionicons name="arrow-back" size={24} color={Colors[theme].text} />}
+                rightIcon={<View style={styles.iconContainer}>
+                    <Ionicons name="notifications-outline" size={20} />
+                </View>}
+                onLeftPress={() => router.replace('/(tabs)')}
+                onRightPress={() => console.log('Notifications/Right pressed')}
+                style={{
+                    marginHorizontal: spacingX._12,
+                    marginBottom: -spacingY._20,
+                }}
+            />
+
+            <BalanceComponent />
+
+            <View style={[styles.contentContainer, { height: height * (Platform.OS == 'ios' ? 0.57 : 0.57), backgroundColor: Colors[theme].secondary }]}>
+
+            </View>
         </Screen>
     );
 };
 
 export default CategoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    contentContainer: {
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        paddingHorizontal: spacingX._30,
+        paddingTop: spacingY._20,
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+    },
+    iconContainer: {
+        backgroundColor: Colors.light.secondaryBtn,
+        padding: spacingX._5,
+        borderRadius: 50,
+    },
+});

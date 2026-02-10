65dd1eea4522ba5473cf48154bd69b425ba07acb
umair <umair.maqbool55@gmail.com>
2026-02-10 21:51:15 +0500

    Terms and condition completed

--- app/(security)/termsAndConditions.tsx
+++ app/(security)/termsAndConditions.tsx
@@ -1,22 +1,32 @@
+import CustomButton from '@/components/CustomButton';
 import Header from '@/components/Header';
 import Screen from '@/components/Screen';
+import { ThemedText } from '@/components/themed-text';
 import { Colors } from '@/constants/theme';
 import { spacingX, spacingY } from '@/constants/theme1';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Ionicons } from '@expo/vector-icons';
-import { router } from 'expo-router';
+import { Checkbox } from 'expo-checkbox';
+import { Link, router } from 'expo-router';
 import React, { useState } from 'react';
-import { Platform, StyleSheet, useWindowDimensions, View } from 'react-native';
+import { Platform, ScrollView, StyleSheet, useWindowDimensions, View } from 'react-native';
+
+const textList = [
+  "Ea voluptates omnis aut sequi sequi.",
+  "Est dolore quae in aliquid ducimus et autem repellendus.",
+  "Aut ipsum Quis qui porro quasi aut minus placeat!",
+  "Sit consequatur neque ab vitae facere.",
+]
+
+const list = [
+  "Aut fuga sequi eum voluptatibus provident.",
+  "Eos consequuntur voluptas vel amet eaque aut dignissimos velit."
+]
 
 const TermsAndConditions = () => {
   const theme = useColorScheme() ?? 'light';
   const { width, height } = useWindowDimensions();
-  const [currentPin, setCurrentPin] = useState('');
-  const [newPin, setNewPin] = useState('');
-  const [confirmPin, setConfirmPin] = useState('');
-  const [showCurrentPin, setShowCurrentPin] = useState(false);
-  const [showNewPin, setShowNewPin] = useState(false);
-  const [showConfirmPin, setShowConfirmPin] = useState(false);
+  const [isChecked, setChecked] = useState(false);
 
   return (
     <Screen style={{ backgroundColor: Colors[theme].primary }}>
@@ -35,10 +45,69 @@ const TermsAndConditions = () => {
         }}
       />
 
-      <View style={[styles.contentContainer, { height: height * (Platform.OS == 'ios' ? 0.80 : 0.80), backgroundColor: Colors[theme].secondary }]}>
+      <ScrollView style={[styles.contentContainer, { height: height * (Platform.OS == 'ios' ? 0.85 : 0.85), backgroundColor: Colors[theme].secondary }]}>
 
+        <ThemedText>
+          Est Fugiat Assumenda Aut Reprehenderit
+        </ThemedText>
 
-      </View>
+        <ThemedText style={styles.textStyle}>
+          Lorem ipsum dolor sit amet. Et odio officia aut voluptate internos est omnis vitae ut architecto sunt non tenetur fuga ut provident vero. Quo aspernatur facere et consectetur ipsum et facere corrupti est asperiores facere. Est fugiat assumenda aut reprehenderit voluptatem sed.
+        </ThemedText>
+
+        <View style={{ marginVertical: spacingY._10 }}>
+          {textList.map((item, index) => (
+            <ThemedText key={index} style={styles.listTextStyle}>
+              {index + 1}.   {item}
+            </ThemedText>
+          ))}
+        </View>
+
+        <ThemedText style={styles.textStyle}>
+          Aut quidem accusantium nam alias autem eum officiis placeat et omnis autem id officiis perspiciatis qui corrupti officia eum aliquam provident. Eum voluptas error et optio dolorum cum molestiae nobis et odit molestiae quo magnam impedit sed fugiat nihil non nihil vitae.
+        </ThemedText>
+
+        <View style={{ marginVertical: spacingY._10 }}>
+          {list.map((item, index) => (
+            <ThemedText key={index} style={styles.listTextStyle}>
+              •   {item}
+            </ThemedText>
+          ))}
+        </View>
+
+        <ThemedText style={styles.textStyle}>
+          Vel exercitationem quam vel eligendi rerum At harum obcaecati et nostrum beatae? Ea accusantium dolores qui rerum aliquam est perferendis mollitia et ipsum ipsa qui enim autem At corporis sunt. Aut odit quisquam est reprehenderit itaque aut accusantium dolor qui neque repellat.
+        </ThemedText>
+
+        <ThemedText style={{ fontSize: 12, marginVertical: spacingY._5, lineHeight: 17 }}>
+          Read the terms and conditions in more detail at
+        </ThemedText>
+
+        <Link href="https://www.finwiseapp.de" target="_blank" style={{ color: Colors.light.focusText, textDecorationLine: 'underline', marginTop: -5 }}>
+          www.finwiseapp.de
+        </Link>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: spacingY._10, gap: 10 }}>
+          <Checkbox
+            // style={styles.checkbox}
+            value={isChecked}
+            onValueChange={setChecked}
+            color={isChecked ? theme === 'light' ? Colors.light.text : Colors.light.primary : undefined}
+          />
+          <ThemedText style={{ fontSize: 12, fontFamily: 'Poppins_400Regular', lineHeight: 17 }}>
+            I accept all the terms and conditions
+          </ThemedText>
+        </View>
+
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <CustomButton
+            title="Accept"
+            onPress={() => router.back()}
+            containerStyle={{ marginTop: spacingY._20 }}
+          />
+        </View>
+
+      </ScrollView>
     </Screen>
   )
 }
@@ -60,31 +129,15 @@ const styles = StyleSheet.create({
     padding: spacingX._5,
     borderRadius: 50,
   },
-  formContainer: {
-    flex: 1,
+  textStyle: {
+    fontSize: 12,
+    fontFamily: 'Poppins_400Regular',
+    marginVertical: spacingY._5,
+    lineHeight: 17
   },
-  label: {
-    fontSize: 14,
-    fontWeight: '400',
-    marginBottom: spacingY._5,
-    marginTop: spacingY._10,
-    marginLeft: spacingX._10,
-  },
-  inputContainer: {
-    borderRadius: 25,
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: spacingX._20,
-    height: 42,
-    marginBottom: spacingY._5,
-  },
-  input: {
-    flex: 1,
-    height: '100%',
-    fontSize: 14,
-    paddingLeft: spacingX._7,
-  },
-  eyeIcon: {
-    padding: 5,
-  },
+  listTextStyle: {
+    fontSize: 12,
+    fontFamily: 'Poppins_400Regular',
+    lineHeight: 17
+  }
 })
